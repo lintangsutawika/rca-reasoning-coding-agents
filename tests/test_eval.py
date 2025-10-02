f46f7ceeea5a4e32b5facd0cd228a59f04dc6ae8
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-10-02 20:38:50 +0000

    test eval

--- tests/test_eval.py
+++ tests/test_eval.py
@@ -1,14 +1,28 @@
+import uuid
 from datasets import load_dataset
-import pytest
+# import pytest
 
-from rca.utils.mini_swe import get_environment, evaluate_trajectory
+from rca.utils.mini_swe import get_sb_environment, evaluate_trajectory
+
+from swesmith.profiles import registry
 
 dataset = load_dataset("SWE-bench/SWE-smith", split="train").to_pandas()
 instance = dataset.iloc[0].to_dict()
 instance["data_source"] = "swe-smith"
-patch = instance["model_patch"]
+instance_id = instance["instance_id"]
+# patch = instance["model_patch"]
+patch = instance["patch"]
 config = {"cwd": "/testbed"}
-env = get_environment(config, instance, "swe-smith")
 
-# evaluate_trajectory
-result = evaluate_trajectory(instance, patch, config, "swe-smith")
+
+
+print("Using ground truth patch:")
+ret = evaluate_trajectory(instance, patch, config, "swe-smith")
+print(ret)
+# {'instance_id': 'oauthlib__oauthlib.1fd52536.combine_file__09vlzwgc', 'resolved': True, 'eval_error': None}
+
+
+print("Using empty patch:")
+ret = evaluate_trajectory(instance, "", config, "swe-smith")
+print(ret)
+# {'instance_id': 'oauthlib__oauthlib.1fd52536.combine_file__09vlzwgc', 'resolved': False, 'eval_error': 'error: unrecognized input\n'}
