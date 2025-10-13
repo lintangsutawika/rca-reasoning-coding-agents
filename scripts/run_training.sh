7cabac9fb2dfba450d9d1ef785179f5ff44f288e
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-10-13 18:53:30 +0000

    update script

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,16 +1,11 @@
 . ./config_env/.env
 
-while getopts ":m:n:t:d:s:f:r:v:g:e:" opt; do
+while getopts ":m:n:d:s:" opt; do
   case ${opt} in
     m ) MODEL=$OPTARG;;
     n ) N_ROLLOUTS=$OPTARG;;
-    t ) TASK=$OPTARG;;
     d ) DATA_PATH=$OPTARG;;
-    s ) SAVE_MODEL_PATH=$OPTARG;;
-    f ) FUNCTION_NAME=$OPTARG;;
-    r ) RUN_NAME=$OPTARG;;
-    g ) USE_GCS=$OPTARG;;
-    e ) SOURCE_TYPE=$OPTARG;;
+    s ) CKPT_PATH=$OPTARG;;
     # \? ) echo "Usage: cmd [-u] [-p]";;
   esac
 done
@@ -18,16 +13,9 @@ done
 MODEL_ALIAS=$(echo $MODEL | sed 's/\//-/g')
 # Get number of GPUs available
 NUM_GPUS=$(nvidia-smi -L | wc -l)
-USE_GCS="${USE_GCS:-False}"
 N_ROLLOUTS="${N_ROLLOUTS:-4}"
-FUNCTION_NAME="${FUNCTION_NAME:-compute_score}"
 MAX_LENGTH=8192
-RUN_NAME="${RUN_NAME:-grpo}"
-RUN_NAME=${RUN_NAME}--${MODEL_ALIAS}--${TASK}
-FULL_DATA_PATH=${DATA_PATH}${TASK}
-FULL_SAVE_PATH=${SAVE_MODEL_PATH}${RUN_NAME}
-LOGPROB_BS=16
-PPO_BS=16
+RUN_NAME="openhands_${MODEL_ALIAS}"
 set -x
 
 # Colocated GRPO training+generation for Qwen3-8B on the SWE-Bench task.
@@ -35,14 +23,12 @@ set -x
 # uv run --isolated examples/mini_swe_agent/preprocess_swegym.py --output_dir ~/data/swe_gym_subset
 # bash examples/mini_swe_agent/run_mini_swe_8B.sh
 
-# DATA_DIR="data/swe_smith"
-# CKPT_PATH="ckpts/${MODEL_ALIAS}"
-DATA_DIR="data/swe_smith"
-CKPT_PATH="ckpts/llm_mini_swe"
-
 # Save trajectories here for debugging
 # NOTE: For a multi-node cluster, ensure that this is on NFS so that you can save all trajectories in the same path
-MINISWE_TRAJ_DIR="mini_swe_agent_trajs"
+
+# DATA_DIR="data/swe_smith"
+DATA_PATH="${DATA_PATH:-data/swe_smith}"
+CKPT_PATH="${CKPT_PATH:-ckpts/${MODEL_ALIAS}}"
 
 NNODES=1
 NUM_INFERENCE_ENGINES=4
@@ -51,17 +37,11 @@ LOGGER=wandb
 
 # We use a small batch size here for demonstration
 # NOTE (sumanthrh): The `generator.max_turns` here is actually unused, and we use the `step_limit` from the `swebench.yaml` file. 
-# This simply has to be a value > 1
-# --isolated --extra vllm --extra miniswe 
-# --env-file examples/mini_swe_agent/.env.miniswe \
-
-# uv run --isolated --extra vllm --extra miniswe --env-file examples/mini_swe_agent/.env.miniswe -m examples.mini_swe_agent.main_mini_swe \
-# uv run --isolated -m mini_swe_agent.main_mini_swe \
 uv run --isolated -m rca.train \
   data.train_data="['$DATA_DIR/train.parquet']" \
   data.val_data="['$DATA_DIR/validation.parquet']" \
   trainer.algorithm.advantage_estimator="grpo" \
-  trainer.policy.model.path="Qwen/Qwen3-1.7B" \
+  trainer.policy.model.path=${MODEL} \
   trainer.placement.colocate_all=true \
   trainer.strategy=fsdp2 \
   trainer.placement.policy_num_gpus_per_node=$NUM_GPUS \
@@ -83,7 +63,7 @@ uv run --isolated -m rca.train \
   trainer.dump_data_batch=true \
   trainer.ckpt_interval=10 \
   trainer.max_prompt_length=4096 \
-  generator.sampling_params.max_generate_length=4096 \
+  generator.sampling_params.max_generate_length=${MAX_LENGTH} \
   generator.max_input_length=30720 \
   generator.max_turns=20 \
   trainer.policy.optimizer_config.lr=1.0e-6 \
@@ -96,13 +76,11 @@ uv run --isolated -m rca.train \
   generator.weight_sync_backend=nccl \
   generator.async_engine=true \
   generator.batched=true \
-  generator.n_samples_per_prompt=1 \
+  generator.n_samples_per_prompt=${N_ROLLOUTS} \
   generator.gpu_memory_utilization=0.8 \
   trainer.logger="$LOGGER" \
-  trainer.project_name="mini_swe" \
-  trainer.run_name="mini_swe_8B_swe_gym" \
+  trainer.project_name="rca" \
+  trainer.run_name=${RUN_NAME} \
   trainer.resume_mode=null \
-  trainer.ckpt_path="$CKPT_PATH" \
-  +generator.miniswe_config_path="mini_swe_agent/swebench.yaml" \
-  +generator.miniswe_traj_dir=$MINISWE_TRAJ_DIR
+  trainer.ckpt_path="$CKPT_PATH"
   $@
