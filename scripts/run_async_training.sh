89507be49d1f98305c04f793dc4b3e663559cb3b
lintangsutawika <lsutawik@andrew.cmu.edu>
2025-12-18 15:35:22 -0500

    update

--- scripts/run_async_training.sh
+++ scripts/run_async_training.sh
@@ -9,7 +9,7 @@
 # NGROK_KEY=${NGROK_KEY}
 # OPENHANDS_RUNTIME_API_KEY=${OPENHANDS_RUNTIME_API_KEY}
 
-while getopts ":m:n:d:s:o:i:t:" opt; do
+while getopts ":m:n:d:s:o:i:t:b:" opt; do
   case ${opt} in
     m ) MODEL=$OPTARG;;
     n ) N_ROLLOUTS=$OPTARG;;
@@ -18,6 +18,7 @@ while getopts ":m:n:d:s:o:i:t:" opt; do
     o ) OTHER_OPTION=$OPTARG;;
     i ) NUM_INFERENCE_ENGINES=$OPTARG;;
     t ) NUM_TRAINING_ENGINES=$OPTARG;;
+    b ) MICRO_BATCH_SIZE=$OPTARG;;
     # \? ) echo "Usage: cmd [-u] [-p]";;
   esac
 done
@@ -28,7 +29,7 @@ NUM_GPUS=$(nvidia-smi -L | wc -l)
 N_ROLLOUTS="${N_ROLLOUTS:-8}"
 BATCH_SIZE=8
 MAX_LENGTH=8192
-RUN_NAME="${MODEL_ALIAS}"
+RUN_NAME="rca_${MODEL_ALIAS}"
 set -x
 
 DATA_PATH="${DATA_PATH:-data/swe_smith}"
@@ -39,7 +40,11 @@ HALF_NUM_GPUS=$((NUM_GPUS / 2))
 NUM_INFERENCE_ENGINES="${NUM_INFERENCE_ENGINES:-$HALF_NUM_GPUS}"
 NUM_TRAINING_ENGINES="${NUM_TRAINING_ENGINES:-$HALF_NUM_GPUS}"
 
-uv run --isolated -m rca.train \
+export VLLM_FLASH_ATTN_VERSION=2
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_USE_CUDA_DSA=1
+
+uv run --isolated -m src.train \
   +run_async_trainer=true \
   data.train_data="['$DATA_PATH/train.parquet']" \
   data.val_data="['$DATA_PATH/validation.parquet']" \
@@ -51,7 +56,7 @@ uv run --isolated -m rca.train \
   trainer.policy.fsdp_config.cpu_offload=true \
   trainer.policy.fsdp_config.reshard_after_forward=true \
   trainer.policy.fsdp_config.fsdp_size=-1 \
-  trainer.fully_async.num_parallel_generation_workers=20 \
+  trainer.fully_async.num_parallel_generation_workers=16 \
   trainer.placement.policy_num_gpus_per_node=${NUM_TRAINING_ENGINES} \
   trainer.placement.ref_num_gpus_per_node=${NUM_TRAINING_ENGINES} \
   trainer.placement.policy_num_nodes=1 \
@@ -59,8 +64,10 @@ uv run --isolated -m rca.train \
   trainer.policy.sequence_parallel_size=1 \
   generator.num_inference_engines=${NUM_INFERENCE_ENGINES} \
   generator.inference_engine_tensor_parallel_size=1 \
-  +generator.traj_dir=$CKPT_PATH/trajectories/ \
-  +generator.engine_init_kwargs="{enable_auto_tool_choice:true,tool_call_parser:hermes,reasoning_parser:qwen3}" \
+  +generator.traj_dir=${CKPT_PATH}trajectories/ \
+  +generator.engine_init_kwargs.enable_auto_tool_choice=true \
+  +generator.engine_init_kwargs.tool_call_parser=hermes \
+  +generator.engine_init_kwargs.reasoning_parser=qwen3 \
   trainer.epochs=20 \
   trainer.eval_batch_size=100 \
   trainer.eval_before_train=false \
@@ -69,13 +76,14 @@ uv run --isolated -m rca.train \
   trainer.train_batch_size=${BATCH_SIZE} \
   trainer.policy_mini_batch_size=${BATCH_SIZE} \
   trainer.micro_forward_batch_size_per_gpu=1 \
-  trainer.micro_train_batch_size_per_gpu=1 \
+  trainer.micro_train_batch_size_per_gpu=${MICRO_BATCH_SIZE:-1} \
   trainer.dump_data_batch=true \
-  trainer.export_path="$CKPT_PATH/exported_model/" \
-  trainer.hf_save_interval=10 \
-  trainer.ckpt_interval=10 \
+  trainer.export_path="${CKPT_PATH}exported_model/" \
+  trainer.hf_save_interval=5 \
+  trainer.ckpt_interval=5 \
   trainer.max_prompt_length=4096 \
   generator.sampling_params.max_generate_length=${MAX_LENGTH} \
+  generator.sampling_params.temperature=1.0 \
   generator.max_input_length=24000 \
   generator.max_num_batched_tokens=48000 \
   generator.max_turns=20 \
@@ -92,10 +100,11 @@ uv run --isolated -m rca.train \
   generator.n_samples_per_prompt=${N_ROLLOUTS} \
   generator.gpu_memory_utilization=0.75 \
   generator.enforce_eager=false \
+  trainer.step_wise_training=true \
   trainer.logger="wandb" \
-  trainer.project_name="rca" \
+  trainer.project_name="code_search" \
   trainer.run_name=${RUN_NAME} \
   trainer.resume_mode=latest \
   trainer.ckpt_path="$CKPT_PATH" \
-  trainer.max_ckpts_to_keep=2 \
+  trainer.max_ckpts_to_keep=3 \
   $OTHER_OPTION
